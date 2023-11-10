fe061df4ad83d78c5b0e4f00da67c545de4d6a33
Reshad Ahmed <reshad4587@gmail.com>
2023-11-10 13:43:18 -0500

    Implemented password complexity checker

--- projects/password-to-sha.py
+++ projects/password-to-sha.py
@@ -8,13 +8,22 @@ def hash_password(password, salt):
     hashed_password = sha256(password.encode('utf-8') + salt).hexdigest()
     return hashed_password, salt
 
+def is_strong_password(password):
+    return len(password) >= 8 and any(c.isupper() for c in password) and any(c.islower() for c in password) and any(c.isdigit() for c in password)
+
 try:
-    input_password = input("Enter a password: ")
-    salt = generate_salt()
-    hashed_password, salt = hash_password(input_password, salt)
-    print(f"Hashed password: {hashed_password}")
-    print(f"Salt: {salt}")
-    
+    while True:
+        input_password = input("Enter a password: ")
+        
+        if is_strong_password(input_password):
+            salt = generate_salt()
+            hashed_password, salt = hash_password(input_password, salt)
+            print(f"Hashed password: {hashed_password}")
+            print(f"Salt: {salt.hex()}")
+            break
+        else:
+            print("Weak password. Please choose a stronger password.")
+            
 except KeyboardInterrupt:
     print("\nExiting script!")
 
